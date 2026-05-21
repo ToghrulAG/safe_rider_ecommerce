1235f28b1adf1564e9f6dc2719aef7b9b2ab4cf3
Brave Cat <agalitogrul14@gmail.com>
2026-05-21 11:39:35 +0300

    Create app_localizations_ru.dart
    Lang translete localization RU

---
+++ mobile/lib/l10n/app_localizations_ru.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Russian (`ru`).
+class AppLocalizationsRu extends AppLocalizations {
+  AppLocalizationsRu([String locale = 'ru']) : super(locale);
+
+  @override
+  String get appName => 'Safe Rider';
+
+  @override
+  String get login => 'Войти';
+
+  @override
+  String get register => 'Регистрация';
+
+  @override
+  String get categories => 'Категории';
+
+  @override
+  String get cart => 'Корзина';
+}
