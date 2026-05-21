93ffb52d5d5c53257688b773740e53952a637f68
Brave Cat <agalitogrul14@gmail.com>
2026-05-21 11:39:27 +0300

    Create app_localizations_tr.dart
    Lang translete localization TR

---
+++ mobile/lib/l10n/app_localizations_tr.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Turkish (`tr`).
+class AppLocalizationsTr extends AppLocalizations {
+  AppLocalizationsTr([String locale = 'tr']) : super(locale);
+
+  @override
+  String get appName => 'Güvenli Sürücü';
+
+  @override
+  String get login => 'Giriş Yap';
+
+  @override
+  String get register => 'Kayıt Ol';
+
+  @override
+  String get categories => 'Kategoriler';
+
+  @override
+  String get cart => 'Sepet';
+}
