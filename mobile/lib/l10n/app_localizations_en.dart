d8f15e2e90ec150b3c423da830c894ab8b7bba79
Brave Cat <agalitogrul14@gmail.com>
2026-05-21 11:39:40 +0300

    Create app_localizations_en.dart
    Lang translete localization EN

---
+++ mobile/lib/l10n/app_localizations_en.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appName => 'Safe Rider';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get categories => 'Categories';
+
+  @override
+  String get cart => 'Cart';
+}
